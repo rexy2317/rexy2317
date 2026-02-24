01f9335282d984b225fcbaa3a31908d2abc5691f
rexy2317 <84739238+rexy2317@users.noreply.github.com>
2026-02-24 14:57:19 +0100

    Remove guild_ids from ciao slash command

--- Trial_Bot.py
+++ Trial_Bot.py
@@ -8,9 +8,10 @@ bot = commands.Bot(intents=discord.Intents.default())
 async def on_ready():
     print(f"Connesso come {bot.user}")
 
-@bot.slash_command(name="ciao",  guild_ids=[1286731492290072738])
+@bot.slash_command(name="ciao")
 async def ciao(ctx):
     await ctx.respond("Ciao! Funziono 🎉")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+
 bot.run(TOKEN)
